36f77122d796d104769804b1d875d55d037dd8a2
samantha-teixeira <samerika69@gmail.com>
2022-08-26 11:49:05 +0200

    a

--- main-exo-01.js
+++
@@ -1,8 +0,0 @@
-let prenom = prompt("Quel est votre prénom?");
-let nom = prompt("Quel est votre nom?");
-let personne = new Stagiaire ();
-let personne.prenom = prenom;
-let personne.nom = nom;
-alert("Bonjour"+" "+prenom+" "+nom);
-
-
--- personne-exo-01.js
+++
@@ -1,6 +0,0 @@
-class Stagiaire
-{
-    prenom;
-    nom;
-
-}
